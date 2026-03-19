002ea5f89fb287558d2f7174b62540beef18f2ba
NikolaDjerov <126071907+NikolaDjerov@users.noreply.github.com>
2026-03-19 11:37:00 +0200

    ss

---
+++ HouseRentingSystem/HouseRentingSystem/Controllers/HouseController.cs
@@ -0,0 +1,32 @@
+﻿using HouseRentingSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+    public class HouseController : Controller
+    {
+        private List<HouseViewModel> houses = new List<HouseViewModel>()
+        {
+            new HouseViewModel()
+            {
+                Name = "Beach house",
+                Address = "Miami, Florida",
+                ImageUrl = "@https://sdchouseplans.com/wp-content/uploads/2025/04/image5.jpg"
+            },
+            new HouseViewModel()
+            {
+                Name = "Mountain house",
+                Address = "Rila Mountain, Bulgaria",
+                ImageUrl = "@https://bghike.com/en/images/huts_pic/rila_lakes_main.jpg"
+            }
+        };
+        public IActionResult AllHouses()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            return View(houses.FirstOrDefault(h=>h.Id == id);
+        }
+    }
+}
